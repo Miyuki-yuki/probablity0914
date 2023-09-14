ce7807311976de73840be6857ffcea2cac077236
Miyuki <87884079+miyuki-KK@users.noreply.github.com>
2023-09-14 23:35:08 +0900

    Initial Commit

---
+++ probability0914/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  probability0914
+//
+//  Created by Miyuki Kikuchi on 14/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ probability0914/probability0914App.swift
@@ -0,0 +1,17 @@
+//
+//  probability0914App.swift
+//  probability0914
+//
+//  Created by Miyuki Kikuchi on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct probability0914App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
